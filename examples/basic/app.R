fbda520b4c3a4b327e015d3158cb1f0e6b0190d3
Filip Stachura <filip@appsilon.pl>
2016-12-05 15:46:21 +0100

    Second demo and comments added. (#4)

--- examples/basic/app.R
+++ examples/basic/app.R
@@ -1,57 +1,42 @@
 library(shiny)
-library(semanticui)
 library(shinyrouter)
 
-# Hint: semanticui
-#
-# Div classes in the UI of this applications are created for semanticui package.
-# Thanks to that we get nice looking interface in our application.
-# Read more: https://github.com/Appsilon/semanticui
-
+# This generates menu in user interface with links.
 menu <- (
-  div(class = "ui vertical menu",
-    div(class = "item",
-      div(class = "header", "Demo"),
-      div(class = "menu",
-        a(class = "item", href = "/", uiicon("home"), "Page"),
-        a(class = "item", href = "/other", uiicon("clock"), "Other")
-      )
-    )
+  tags$ul(
+    tags$li(a(class = "item", href = "/", "Page")),
+    tags$li(a(class = "item", href = "/other", "Other page"))
   )
 )
 
+# This creates UI for each page.
 page <- function(title, content) {
-  div(class = "ui container",
-    style = "margin-top: 1em",
-    div(class = "ui grid",
-      div(class = "four wide column",
-        menu
-      ),
-      div(class = "twelve wide column",
-        div(class = "ui segment",
-          h1(title),
-          p(content)
-        )
-      )
-    )
+  div(
+    menu,
+    titlePanel(title),
+    p(content)
   )
 }
 
+# Both sample pages.
 root_page <- page("Home page", "Welcome on sample routing page!")
 other_page <- page("Some other page", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 
+# Creates router. We provide routing path and UI for this page.
 router <- make_router(
   route("/", root_page),
   route("/other", other_page)
 )
 
-ui <- shinyUI(semanticPage(
-  title = "Router demo",
+# Creat output for our router in main UI of Shiny app.
+ui <- shinyUI(fluidPage(
   router_ui()
 ))
 
+# Plug router into Shiny server.
 server <- shinyServer(function(input, output) {
   router(input, output)
 })
 
+# Run server in a standard way.
 shinyApp(ui, server)
---
+++ examples/semanticui/app.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(semanticui)
+library(shinyrouter)
+
+# Hint: semanticui
+#
+# Div classes in the UI of this applications are created for semanticui package.
+# Thanks to that we get nice looking interface in our application.
+# Read more: https://github.com/Appsilon/semanticui
+
+menu <- (
+  div(class = "ui vertical menu",
+    div(class = "item",
+      div(class = "header", "Demo"),
+      div(class = "menu",
+        a(class = "item", href = "/", uiicon("home"), "Page"),
+        a(class = "item", href = "/other", uiicon("clock"), "Other")
+      )
+    )
+  )
+)
+
+page <- function(title, content) {
+  div(class = "ui container",
+    style = "margin-top: 1em",
+    div(class = "ui grid",
+      div(class = "four wide column",
+        menu
+      ),
+      div(class = "twelve wide column",
+        div(class = "ui segment",
+          h1(title),
+          p(content)
+        )
+      )
+    )
+  )
+}
+
+root_page <- page("Home page", "Welcome on sample routing page!")
+other_page <- page("Some other page", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+router <- make_router(
+  route("/", root_page),
+  route("/other", other_page)
+)
+
+ui <- shinyUI(semanticPage(
+  title = "Router demo",
+  router_ui()
+))
+
+server <- shinyServer(function(input, output) {
+  router(input, output)
+})
+
+shinyApp(ui, server)
