78c6e664497558244ce0f8aac019fed6b9dfef8d
Robin Neumann <robin.neumann@posteo.de>
2018-04-09 18:20:49 +0200

    Add missing empty line

--- R/router.R
+++ R/router.R
@@ -28,6 +28,7 @@ valid_path <- function(routes, path) {
 
 #' Create a mapping bewtween a ui element
 #' and a server callack
+#'
 #' @param ui Valid Shiny user interface.
 #' @param server Function that is called within the global server function if given
 callback_mapping <- function(ui, server = NA) {
