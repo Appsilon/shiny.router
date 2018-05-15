af483ba7d5de641fa3b3e46a3ff8cbc5920e77e1
Krystian Igras <krystian8207@gmail.com>
2018-05-15 16:00:02 +0200

    Add unittests

--- tests/testthat/test-router.R
+++ tests/testthat/test-router.R
@@ -2,8 +2,17 @@ context("router")
 
 ## TODO: Add more tests
 
-test_that("test route", {
-  rr <- route("aa", shiny::div("a"))
-  expect_equal(rr$aa, shiny::div("a"))
+test_that("test route without server", {
+  ui <- shiny::div("a")
+  server <- function(input, output){}
+  rr <- route("aa", ui)
+  expect_equal(rr$aa, list(ui = ui, server = server))
   expect_error(route("aa"))
+})
+
+test_that("test route with server", {
+  ui <- shiny::div("a")
+  server <- function(input, output) {output$val <- renderText("Hello")}
+  rr <- route("aa", ui, server)
+  expect_equal(rr$aa, list(ui = ui, server = server))
 })
