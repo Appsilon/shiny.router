c268c57ad8e0126398aee6b4c5d1d0627e6b89fd
Jakub Nowicki <kuba@appsilon.com>
2023-02-16 11:06:12 +0100

    chore: Update .onAttach message.

--- R/zzz.R
+++ R/zzz.R
@@ -7,8 +7,7 @@
 #'
 #' @keywords internal
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("shiny.router from version >= 0.2.0 introduced major
-  changes to the API that are not compatible with the
-  previous versions. Please check the docs, or examples
-  to learn more: https://github.com/Appsilon/shiny.router")
+  packageStartupMessage("shiny.router from version > 0.2.3 introduced major
+  changes to the API. Old API is still working but has been marked as deprecated.
+  Please check the docs, or examples to learn more: https://github.com/Appsilon/shiny.router")
 }
